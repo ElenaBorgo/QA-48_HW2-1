c9e6a2eb96ca165c8cf73eb5bde449ae38850e4e
Elena Borgoiakova <borgoiakova.elen888@gmail.com>
2022-10-20 18:06:19 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 100;
+        int cashForBonus = 20;
+
+        int bonus = ticket / cashForBonus;
+        System.out.println("Количество бонусных миль: " + bonus);
+    }
+
+}
